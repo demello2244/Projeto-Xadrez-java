a42c04f53c4c58af54a77505ab5f8ca2aaddcb05
Denise Mello <denisemello.zero@gmail.com>
2021-01-13 18:42:00 -0300

    Little improvement in board printing 2

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,17 +1,9 @@
 package chess;
 
-<<<<<<< HEAD
-public class ChessException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-	
-	public ChessException(String msg) {
-=======
 public class ChessException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
 	public ChessException (String msg) {
->>>>>>> 605590c79a44198c7be2039daa1897b71b7964d2
 		super(msg);
 	}
 
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,7 +3,6 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
-<<<<<<< HEAD
 
 	private char column;
 	private int row;
@@ -11,14 +10,6 @@ public class ChessPosition {
 	public ChessPosition(char column, int row) {
 		if (column < 'A' || column > 'H' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from A1 to H8.");
-=======
-	private char column;
-	private int row;
-	
-	public ChessPosition(char column, int row) {
-		if(column<'A' || column >'H' || row<0 || row>8) {
-			throw new ChessException("Error instantiating ChessPosition. Valid values arte from A1 to H8");
->>>>>>> 605590c79a44198c7be2039daa1897b71b7964d2
 		}
 		this.column = column;
 		this.row = row;
@@ -31,21 +22,6 @@ public class ChessPosition {
 	public int getRow() {
 		return row;
 	}
-<<<<<<< HEAD
-
-	protected Position toPosition() {
-		return new Position(8 - row, column - 'A');
-	}
-
-	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char) ('A' - position.getColumn()), 8 - position.getRow());
-	}
-
-	@Override
-	public String toString() {
-		return "" + column + row;
-	}
-=======
 	
 	protected Position toPosition() {
 		return new Position(8-row, column -'A');
@@ -61,5 +37,4 @@ public class ChessPosition {
 		return ""+column+row;
 	}
 
->>>>>>> 605590c79a44198c7be2039daa1897b71b7964d2
 }
